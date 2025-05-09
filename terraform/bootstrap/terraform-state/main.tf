d45cbd170a2887ede8cc8640d428e187ecb11a7b
Nasir Noor <nasir@nasir.id>
2025-05-09 22:25:46 +0700

    update

---
+++ terraform/bootstrap/terraform-state/main.tf
@@ -0,0 +1,7 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket        = "devops-aws-terraform-docker-actions-terraform-state"
+
+  force_destroy       = true
+  object_lock_enabled = false
+  # tags                = "terraform-state"
+}
