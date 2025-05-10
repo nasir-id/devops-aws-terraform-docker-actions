652053ae4a495ae7acc074340cab531a8210c5d4
Nasir Noor <nasir@nasir.id>
2025-05-10 23:38:27 +0700

    sg

--- terraform/services/ec2/ec2.tf
+++ terraform/services/ec2/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "webapp_server" {
   ami                    = "ami-0c1907b6d738188e5" 
   instance_type          = "t2.micro"
   subnet_id              = data.terraform_remote_state.subnet.outputs.subnet_id
-  vpc_security_group_ids = [aws_security_group.allow_http.id]
+  vpc_security_group_ids = [aws_security_group.allow_http.id, aws_security_group.allow_5000.id, aws_security_group.allow_ssh.id]
   iam_instance_profile   = aws_iam_instance_profile.webapp.name
   availability_zone           = "ap-southeast-1a"
   user_data = file("user_data.sh")
