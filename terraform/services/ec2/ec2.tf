6dc2b23f423bc9b61a46da94efa4caed393a7770
Nasir Noor <nasir@nasir.id>
2025-05-11 09:27:36 +0700

    update ec2 instance type

--- terraform/services/ec2/ec2.tf
+++ terraform/services/ec2/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "webapp_server" {
   ami                    = "ami-0c1907b6d738188e5" 
-  instance_type          = "t2.small"
+  instance_type          = "t3.micro"
   subnet_id              = data.terraform_remote_state.subnet.outputs.subnet_id
   vpc_security_group_ids = [aws_security_group.allow_http.id, aws_security_group.allow_5000.id, aws_security_group.allow_ssh.id]
   iam_instance_profile   = aws_iam_instance_profile.webapp.name
